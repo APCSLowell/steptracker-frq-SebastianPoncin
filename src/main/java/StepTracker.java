1a17402655b1881de65e253c657d49a3178da9a2
Sebastian <103226759+SebastianPoncin@users.noreply.github.com>
2025-04-23 09:14:42 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,35 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+  private ArrayList<Integer> steps = new ArrayList<Integer>();
+  private int minSteps;
+  
+  public StepTracker(int n) {
+     minSteps = n;
+  }
+  
+  public int activeDays() {
+    int count = 0;
+    for(int i = 0; i < steps.size(); i++) {
+        if(steps.get(i) > minSteps) {
+          count += 1;  
+        }
+    }
+    return count;
+  }
+  
+  public void addDailySteps(int n) {
+    steps.add(n);
+  }
+  
+  public double averageSteps() {
+    if(steps.size() > 0) {
+      int sum = 0;
+      for(int i = 0; i < steps.size(); i++) {
+        sum += steps.get(i);  
+      }
+      return (double)sum/((double)steps.size());
+    }
+    return 0;
+  }
 } 
